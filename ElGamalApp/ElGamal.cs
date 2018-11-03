89638c5b6f09cd803a423bcc8b5e956aeeb62962
PMS <sarakuqi@hotmail.com>
2018-11-03 17:39:13 +0100

    Add project files.

---
+++ ElGamalApp/ElGamal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElGamalApp
+{
+    public abstract class ElGamal : AsymmetricAlgorithm
+    {
+        public abstract void ImportParameters(ElGamalParameters p_parameters);
+        public abstract ElGamalParameters ExportParameters(bool
+            p_include_private_params);
+    }
+}
