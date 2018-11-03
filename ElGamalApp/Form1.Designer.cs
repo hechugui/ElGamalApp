6ad99ae11a1f21880487aab99f75701c801c7f30
Muhamed <muhamed.r.krasniqi@gmail.com>
2018-11-03 20:53:02 +0100

    Encrypt/Decrypt functions

--- ElGamalApp/Form1.Designer.cs
+++ ElGamalApp/Form1.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CipherText = new System.Windows.Forms.Label();
+            this.lblCipherText = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.PlainText = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblPlaintext = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // CipherText
+            // 
+            this.CipherText.AutoSize = true;
+            this.CipherText.Location = new System.Drawing.Point(63, 81);
+            this.CipherText.Name = "CipherText";
+            this.CipherText.Size = new System.Drawing.Size(61, 13);
+            this.CipherText.TabIndex = 0;
+            this.CipherText.Text = "CipherText:";
+            // 
+            // lblCipherText
+            // 
+            this.lblCipherText.AutoSize = true;
+            this.lblCipherText.Location = new System.Drawing.Point(130, 81);
+            this.lblCipherText.Name = "lblCipherText";
+            this.lblCipherText.Size = new System.Drawing.Size(19, 13);
+            this.lblCipherText.TabIndex = 1;
+            this.lblCipherText.Text = "__";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(133, 31);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(238, 20);
+            this.txtPlainText.TabIndex = 2;
+            // 
+            // PlainText
+            // 
+            this.PlainText.AutoSize = true;
+            this.PlainText.Location = new System.Drawing.Point(63, 38);
+            this.PlainText.Name = "PlainText";
+            this.PlainText.Size = new System.Drawing.Size(54, 13);
+            this.PlainText.TabIndex = 3;
+            this.PlainText.Text = "PlainText:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(66, 158);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(109, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "Encrypt/Decrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // lblPlaintext
+            // 
+            this.lblPlaintext.AutoSize = true;
+            this.lblPlaintext.Location = new System.Drawing.Point(130, 120);
+            this.lblPlaintext.Name = "lblPlaintext";
+            this.lblPlaintext.Size = new System.Drawing.Size(19, 13);
+            this.lblPlaintext.TabIndex = 6;
+            this.lblPlaintext.Text = "__";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PlainText:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 423);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPlaintext);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.PlainText);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.lblCipherText);
+            this.Controls.Add(this.CipherText);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label CipherText;
+        private System.Windows.Forms.Label lblCipherText;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label PlainText;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblPlaintext;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ElGamalApp/Form1.cs
+++ ElGamalApp/Form1.cs
@@ -17,5 +17,28 @@ namespace ElGamalApp
             InitializeComponent();
 
         }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            byte[] x_plaintext = Encoding.Default.GetBytes(txtPlainText.Text);
+            ElGamal x_alg = new ElGamalManaged();
+            // set the key size - keep is small to speed up the tests
+            x_alg.KeySize = 384;
+            // extract and print the xml string (this will cause
+            // a new key pair to be generated)
+            string x_xml_string = x_alg.ToXmlString(true);
+            ElGamal x_encrypt_alg = new ElGamalManaged();
+            x_encrypt_alg.FromXmlString(x_alg.ToXmlString(false));
+            byte[] x_ciphertext = x_alg.EncryptData(x_plaintext);
+            lblCipherText.Text = Encoding.UTF8.GetString(x_ciphertext);
+
+            ElGamal x_decrypt_alg = new ElGamalManaged();
+            // set the keys - note that we export with the
+            // private parameters since we are decrypting data
+            x_decrypt_alg.FromXmlString(x_alg.ToXmlString(true));
+            // restore the plaintext
+            byte[] x_candidate_plaintext = x_decrypt_alg.DecryptData(x_ciphertext);
+            lblPlaintext.Text = Encoding.UTF8.GetString(x_candidate_plaintext);
+        }
     }
 }
