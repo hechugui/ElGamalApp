92f17df9f99d040a8fa332c16818bfa32d669374
Dardan Sojeva <dardann@msn.com>
2018-11-16 16:15:19 +0100

    //

--- ElGamalApp/ElGamalDecryptor.cs
+++ ElGamalApp/ElGamalDecryptor.cs
@@ -17,18 +17,21 @@ namespace ElGamalApp
             byte[] x_a_bytes = new byte[o_ciphertext_blocksize / 2];
             Array.Copy(p_block, 0, x_a_bytes, 0, x_a_bytes.Length);
             byte[] x_b_bytes = new Byte[o_ciphertext_blocksize / 2];
+
             Array.Copy(p_block, x_a_bytes.Length, x_b_bytes, 0, x_b_bytes.Length);
+
             BigInteger A = new BigInteger(x_a_bytes);
+
             BigInteger B = new BigInteger(x_b_bytes);
-            BigInteger M = (B *
-                A.modPow(o_key_struct.X, o_key_struct.P).modInverse(o_key_struct.P))% o_key_struct.P;
+
+            BigInteger M = (B * A.modPow(o_key_struct.X, o_key_struct.P).modInverse(o_key_struct.P))% o_key_struct.P;
+
             byte[] x_m_bytes = M.getBytes();
 
             if (x_m_bytes.Length < o_plaintext_blocksize)
             {
                 byte[] x_full_result = new byte[o_plaintext_blocksize];
-                Array.Copy(x_m_bytes, 0, x_full_result,
-                    o_plaintext_blocksize - x_m_bytes.Length, x_m_bytes.Length);
+                Array.Copy(x_m_bytes, 0, x_full_result, o_plaintext_blocksize - x_m_bytes.Length, x_m_bytes.Length);
                 x_m_bytes = x_full_result;
             }
             return x_m_bytes;
--- ElGamalApp/ElGamalSignature.cs
+++ ElGamalApp/ElGamalSignature.cs
@@ -28,19 +28,20 @@ namespace ElGamalApp
             } while (K.gcd(x_pminusone) != 1);
 
             BigInteger A = p_key_struct.G.modPow(K, p_key_struct.P);
-            BigInteger B = mod(mod(K.modInverse(x_pminusone)
-                * (new BigInteger(p_data)
-                - (p_key_struct.X * (A))), (x_pminusone)), (x_pminusone));
+            BigInteger B = mod(mod(K.modInverse(x_pminusone)* (new BigInteger(p_data)- (p_key_struct.X * (A))), (x_pminusone)), (x_pminusone));
+
             byte[] x_a_bytes = A.getBytes();
             byte[] x_b_bytes = B.getBytes();
+
             int x_result_size = (((p_key_struct.P.bitCount() + 7) / 8) * 2);
             byte[] x_result = new byte[x_result_size];
-            Array.Copy(x_a_bytes, 0, x_result, x_result_size / 2
-                - x_a_bytes.Length, x_a_bytes.Length);
-            Array.Copy(x_b_bytes, 0, x_result, x_result_size
-                - x_b_bytes.Length, x_b_bytes.Length);
+
+            Array.Copy(x_a_bytes, 0, x_result, x_result_size / 2 - x_a_bytes.Length, x_a_bytes.Length);
+            Array.Copy(x_b_bytes, 0, x_result, x_result_size - x_b_bytes.Length, x_b_bytes.Length);
+
             return x_result;
         }
+
         public static bool VerifySignature(byte[] p_data, byte[] p_sig, ElGamalKeyStruct p_key_struct)
         {
             int x_result_size = p_sig.Length / 2;
@@ -52,11 +53,9 @@ namespace ElGamalApp
 
             BigInteger A = new BigInteger(x_a_bytes);
             BigInteger B = new BigInteger(x_b_bytes);
-            BigInteger x_result1 = mod(p_key_struct.Y.modPow(A, p_key_struct.P)
-            * A.modPow(B, p_key_struct.P), p_key_struct.P);
+            BigInteger x_result1 = mod(p_key_struct.Y.modPow(A, p_key_struct.P) * A.modPow(B, p_key_struct.P), p_key_struct.P);
 
-            BigInteger x_result2 = p_key_struct.G.modPow(new BigInteger(p_data),
-                p_key_struct.P);
+            BigInteger x_result2 = p_key_struct.G.modPow(new BigInteger(p_data), p_key_struct.P);
 
             return x_result1 == x_result2;
         }
